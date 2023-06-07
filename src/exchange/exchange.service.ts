9ba10bb6d16d00c0e24238b48c90f2fdc24a7cc3
lghiggino <lghiggino@gmail.com>
2023-06-07 08:01:16 -0300

    test: ensure convertAmount breaks with invalid params

--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ExchangeService {}
+export class ExchangeService {
+  async convertAmount({ from, to, amount }): Promise<any> {
+    if (!from || !to || !amount) {
+      throw new Error();
+    }
+  }
+}
